0c83fa9e9e35be87007c64dc6cebfdc2a7720fbb
unknown <karan.funkyrohit29@gmail.com>
2020-07-04 04:18:05 +0530

    Website up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:150]
+
+    def showdate(self):
+        return self.pub_date.strftime('%b %e %Y')
